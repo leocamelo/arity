390a9a87cb4973c41f7dc2c7bec54f08164c06c8
leocamelo <leonardocamelo.nave@gmail.com>
2019-07-11 14:59:47 -0300

    audit

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const rename = require('gulp-rename');
 
 const src = 'arity.js';
 
-gulp.task('uglify', () => {
-  gulp.src(src)
-  .pipe(uglify())
-  .pipe(rename({ extname: '.min.js' }))
-  .pipe(gulp.dest('./'));
-});
+function js() {
+  return gulp
+    .src(src)
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./'));
+}
 
-gulp.task('default', ['uglify'], () => {
-  gulp.watch(src, ['uglify']);
-});
+exports.js = js;
+exports.default = js;
